d671ff3e630ea5ddbd2873a59ff69a8bb032f20d
Zeki Ahmed <changezworld3@gmail.com>
2026-02-04 16:28:03 +0300

    web search tool and web search sources done

---
+++ nextjs-aisdk-app/app/api/web-search-tool/route.ts
@@ -0,0 +1,42 @@
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  InferUITools,
+  UIDataTypes,
+  stepCountIs,
+  type ToolSet,
+} from "ai";
+// import { openai } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
+
+const tools = {
+  // web_search_preview: openai.tools.webSearchPreview({}),
+  web_search: anthropic.tools.webSearch_20250305({
+    maxUses: 1,
+  }),
+}
+
+export type ChatTools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, ChatTools>;
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: ChatMessage[] } = await req.json();
+
+    const result = streamText({
+      // model: openai.responses("gpt-5-nano"),
+      model: anthropic("claude-sonnet-4-20250514"),
+      messages: await convertToModelMessages(messages),
+      tools,
+      stopWhen: stepCountIs(2),
+    });
+
+    return result.toUIMessageStreamResponse({
+      sendSources: true,
+    });
+  } catch (error) {
+    console.error("Error streaming chat completion:", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
+}
